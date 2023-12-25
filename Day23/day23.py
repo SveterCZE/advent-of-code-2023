cc4d60185513da9050c57bf9fae165cb9dd3fa08
SveterCZE <petr.smerkl@live.com>
2023-12-25 11:17:17 +0100

    day 23, complete

--- Day23/day23.py
+++ Day23/day23.py
@@ -4,7 +4,7 @@ def main():
     instructions = get_input()
     print(part1(instructions))
     modify_the_map(instructions)
-    print(part1(instructions))
+    print(part2(instructions))
 
 def get_input():
     coordinates = set()
@@ -23,7 +23,85 @@ def part1(instructions):
     make_steps(start_coord, visited_coordinates, instructions, finished_journeys_DB, finish_coord)
     finished_journeys_DB.sort()
     return finished_journeys_DB[-1]
+
+def part2(instructions):
+    start_coord, finish_coord = find_start_and_finish(instructions)
+    finished_journeys_DB = []
+    intersections = find_intersections(instructions)
+    intersections.add(start_coord)
+    intersections.add(finish_coord)
+    graph = build_graph(instructions, intersections)
+    find_all_journeys_in_graph(start_coord, finish_coord, graph, finished_journeys_DB)
+    finished_journeys_DB.sort()
+    return finished_journeys_DB[-1]
+
+def find_all_journeys_in_graph(start_coord, finish_coord, graph, finished_journeys_DB):
+    journey_length = 0
+    visited_nodes = set()
+    visited_nodes.add(start_coord)
+    find_all_journeys_in_graph_recursive_helper(start_coord, finish_coord, graph, finished_journeys_DB, visited_nodes, journey_length)
+
+def find_all_journeys_in_graph_recursive_helper(current_coord, finish_coord, graph, finished_journeys_DB, visited_nodes, journey_length):
+    # BASE CASE --- Reached the finish
+    if finish_coord in visited_nodes:
+        finished_journeys_DB.append(journey_length)
+        return
+    # RECURSIVE CASE --- Continue exploring
+    else:
+        for next_node, distance in graph[current_coord].items():
+            if next_node not in visited_nodes:
+                updated_journey = copy.deepcopy(visited_nodes)
+                updated_journey.add(next_node)
+                find_all_journeys_in_graph_recursive_helper(next_node, finish_coord, graph, finished_journeys_DB, updated_journey, journey_length + distance)
+
+
+def build_graph(instructions, intersections):
+    graph = {}
+    for initial_coordinate in intersections:
+        graph[initial_coordinate] = {}
+        visited_coordinates = set()
+        visited_coordinates.add(initial_coordinate)
+        make_steps_build_graph(initial_coordinate, initial_coordinate, visited_coordinates, instructions, graph, intersections)
+    return graph
+
+def make_steps_build_graph(initial_coordinate, current_coordinate, visited_coordinates, instructions, graph, intersections):
+    # BASE CASE --- Found other intersection
+    if current_coordinate in intersections and current_coordinate != initial_coordinate:
+        graph[initial_coordinate][current_coordinate] = len(visited_coordinates) - 1
+        return 
     
+    # RECURSIVE CASE --- Make new steps
+    possible_next_steps = find_possible_next_steps(current_coordinate, instructions, visited_coordinates)
+    for possible_step in possible_next_steps:
+        new_visited_coord_DB = copy.deepcopy(visited_coordinates)
+        new_visited_coord_DB.add(possible_step)
+        make_steps_build_graph(initial_coordinate, possible_step, new_visited_coord_DB, instructions, graph, intersections)
+
+def find_intersections(instructions):
+    intersections = set()
+    for row in range(1, len(instructions) - 1):
+        for column in range(1, len(instructions[0]) - 1):
+            if instructions[row][column] == ".":
+                if count_neighbouring_roads((row, column), instructions) > 2:
+                    intersections.add((row, column))
+    return intersections
+
+def count_neighbouring_roads(checked_coordinate, instructions):
+    counter = 0
+    N = (checked_coordinate[0] - 1, checked_coordinate[1])
+    if instructions[N[0]][N[1]] == ".":
+        counter += 1
+    S = (checked_coordinate[0] + 1, checked_coordinate[1])
+    if instructions[S[0]][S[1]] == ".":
+        counter += 1
+    E = (checked_coordinate[0], checked_coordinate[1] + 1)
+    if instructions[E[0]][E[1]] == ".":
+        counter += 1
+    W = (checked_coordinate[0], checked_coordinate[1] - 1)
+    if instructions[W[0]][W[1]] == ".":
+        counter += 1
+    return counter
+
 def make_steps(current_coordinate, visited_coordinates, instructions, finished_journeys_DB, finish_coord):
     # BASE CASE --- Reached the final coordinate
     if is_finish_found(visited_coordinates, finish_coord):
@@ -72,7 +150,7 @@ def find_possible_next_steps(current_coordinate, instructions, visited_coordinat
 def is_valid_coordinate(checked_coordinate, instructions, special_char, visited_coordinates):
     if checked_coordinate in visited_coordinates:
         return False
-    if checked_coordinate[0] < 0 or checked_coordinate[1] < 0:
+    if checked_coordinate[0] < 0 or checked_coordinate[1] < 0 or checked_coordinate[0] >= len(instructions) or checked_coordinate[0] >= len(instructions[0]):
         return False
     if instructions[checked_coordinate[0]][checked_coordinate[1]] == "." or instructions[checked_coordinate[0]][checked_coordinate[1]] == special_char:
         return True
