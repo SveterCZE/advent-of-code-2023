a52af6e618f7e2c9fc0f76a9a417638452dad249
SveterCZE <petr.smerkl@live.com>
2024-01-08 22:36:51 +0100

    day 7, part 1 refactoring

--- Day7/day7.py
+++ Day7/day7.py
@@ -34,13 +34,13 @@ def prepare_card_values(hand):
 
 def part1(instructions):
     sorted_cards = []
-    sorted_cards += extract_singles(instructions, sorted_cards)
-    sorted_cards += extract_one_pair(instructions, sorted_cards)
-    sorted_cards += extract_two_pairs(instructions, sorted_cards)
-    sorted_cards += extract_three(instructions, sorted_cards)
-    sorted_cards += extract_full_house(instructions, sorted_cards)
-    sorted_cards += extract_fours(instructions, sorted_cards)
-    sorted_cards += extract_fives(instructions, sorted_cards)
+    sorted_cards += extract_singles(instructions)
+    sorted_cards += extract_one_pair(instructions)
+    sorted_cards += extract_two_pairs(instructions)
+    sorted_cards += extract_three(instructions)
+    sorted_cards += extract_full_house(instructions)
+    sorted_cards += extract_fours(instructions)
+    sorted_cards += extract_fives(instructions)
     return count_card_winnings(sorted_cards)
 
 def part2(instructions):
@@ -52,75 +52,62 @@ def count_card_winnings(sorted_cards):
         winnings += (sorted_cards[i][-1] * (i+1))
     return winnings
 
-def extract_fives(instructions, sorted_cards):
+def extract_fives(instructions):
     fives = []
     for checked_card in instructions:
         if len(checked_card[0]) == 1:
-            fives_hand = checked_card[2]
-            fives_hand.append(checked_card[1])
-            fives.append(fives_hand)
-    fives = sorted(fives, key=itemgetter(0,1,2,3,4))
-    return fives
+            add_found_card_hand(checked_card, fives)
+    return sorted_list(fives)
 
-def extract_fours(instructions, sorted_cards):
+def extract_fours(instructions):
     fours = []
     for checked_card in instructions:
         if(len(checked_card[0])) == 2 and 4 in list(checked_card[0].values()):
-            fours_hand = checked_card[2]
-            fours_hand.append(checked_card[1])
-            fours.append(fours_hand)
-    fours = sorted(fours, key=itemgetter(0,1,2,3,4))
-    return fours
+            add_found_card_hand(checked_card, fours)
+    return sorted_list(fours)
 
-def extract_full_house(instructions, sorted_cards):
+def extract_full_house(instructions):
     full_houses = []
     for checked_card in instructions:
         if(len(checked_card[0])) == 2 and 3 in list(checked_card[0].values()):     
-            full_house_hand = checked_card[2]
-            full_house_hand.append(checked_card[1])
-            full_houses.append(full_house_hand)
-    full_houses = sorted(full_houses, key=itemgetter(0,1,2,3,4))
-    return full_houses
+            add_found_card_hand(checked_card, full_houses)
+    return sorted_list(full_houses)
 
-def extract_three(instructions, sorted_cards):
+def extract_three(instructions):
     threes = []
     for checked_card in instructions:
         if(len(checked_card[0])) == 3 and 3 in list(checked_card[0].values()):
-            three_hands = checked_card[2]
-            three_hands.append(checked_card[1])
-            threes.append(three_hands)
-    threes = sorted(threes, key=itemgetter(0,1,2,3,4))
-    return threes
+            add_found_card_hand(checked_card, threes)
+    return sorted_list(threes)
 
-def extract_two_pairs(instructions, sorted_cards):
+def extract_two_pairs(instructions):
     two_pairs = []
     for checked_card in instructions:
         if(len(checked_card[0])) == 3 and 2 in list(checked_card[0].values()):
-            two_hands = checked_card[2]
-            two_hands.append(checked_card[1])
-            two_pairs.append(two_hands)
-    two_pairs = sorted(two_pairs, key=itemgetter(0,1,2,3,4))
-    return two_pairs
+            add_found_card_hand(checked_card, two_pairs)
+    return sorted_list(two_pairs)
 
-def extract_one_pair(instructions, sorted_cards):
+def extract_one_pair(instructions):
     one_pair = []
     for checked_card in instructions:
         if(len(checked_card[0])) == 4:
-            one_pair_hand = checked_card[2]
-            one_pair_hand.append(checked_card[1])
-            one_pair.append(one_pair_hand)
-    one_pair = sorted(one_pair, key=itemgetter(0,1,2,3,4))
-    return one_pair
+            add_found_card_hand(checked_card, one_pair)
+    return sorted_list(one_pair)
 
-def extract_singles(instructions, sorted_cards):
+def extract_singles(instructions):
     singles = []
     for checked_card in instructions:
         if len(checked_card[0]) == 5:
-            singles_hand = checked_card[2]
-            singles_hand.append(checked_card[1])
-            singles.append(singles_hand)
-    singles = sorted(singles, key=itemgetter(0,1,2,3,4))
-    return singles
+            add_found_card_hand(checked_card, singles)
+    return sorted_list(singles)
+
+def sorted_list(list_to_be_sorted):
+    return sorted(list_to_be_sorted, key=itemgetter(0,1,2,3,4))
+
+def add_found_card_hand(checked_card, list_of_cards_found):
+        found_hand = checked_card[2]
+        found_hand.append(checked_card[1])
+        list_of_cards_found.append(found_hand)
 
 if __name__ == "__main__":
     instructions = get_input()
