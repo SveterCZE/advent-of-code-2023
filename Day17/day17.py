335c26e23979a8c8ceda04e11ee122e45e352621
SveterCZE <petr.smerkl@live.com>
2024-09-01 19:37:08 +0200

    day 17, part 2

--- Day17/day17.py
+++ Day17/day17.py
@@ -12,14 +12,12 @@ def get_input():
 def part1(instructions):
     visited_coordinates = set()
     initial_coordinate = (0,0)
-    direction = "S"
-    direction_stepe_taken = 0
     steps_queue = queue.PriorityQueue()
-    steps_queue.put((0, initial_coordinate, direction, direction_stepe_taken))
+    steps_queue.put((0, initial_coordinate, "S", 0))
     heat_loss = run_steps_calculation(instructions, visited_coordinates, steps_queue)
     return heat_loss
 
-def run_steps_calculation(instructions, visited_coordinates, steps_queue):
+def run_steps_calculation(instructions, visited_coordinates, steps_queue, p2=False):
     while True:
         current_coordinate = steps_queue.get()
         if (current_coordinate[1], current_coordinate[2], current_coordinate[3]) in visited_coordinates:
@@ -28,29 +26,30 @@ def run_steps_calculation(instructions, visited_coordinates, steps_queue):
             visited_coordinates.add((current_coordinate[1], current_coordinate[2], current_coordinate[3]))
             # Check if the target had been reached
             if current_coordinate[1] == (len(instructions) - 1, len(instructions[0]) - 1):
-                # print(current_coordinate[3])
-                return current_coordinate[0]
+                if p2 == False:
+                    return current_coordinate[0]
+                if p2 == True:
+                    if current_coordinate[3] >= 4:
+                        return current_coordinate[0]
             # If not, generate next possible steps and add if they are valid, add them to the queue
-            generate_potential_next_steps(current_coordinate, instructions, visited_coordinates, steps_queue)
+            generate_potential_next_steps(current_coordinate, instructions, steps_queue, p2)
 
         
-def generate_potential_next_steps(current_coordinate, instructions, visited_coordinates, steps_queue):
-    if is_move_direction_permissible(current_coordinate[2], current_coordinate[3], "N"):
+def generate_potential_next_steps(current_coordinate, instructions, steps_queue, p2):
+    if is_move_direction_permissible(current_coordinate[2], current_coordinate[3], "N", p2):
         N = (current_coordinate[1][0] - 1, current_coordinate[1][1])
         try_adding_step_to_queue(current_coordinate, N, "N", instructions, steps_queue)
-    if is_move_direction_permissible(current_coordinate[2], current_coordinate[3], "E"):
+    if is_move_direction_permissible(current_coordinate[2], current_coordinate[3], "E", p2):
         E = (current_coordinate[1][0], current_coordinate[1][1] + 1)
         try_adding_step_to_queue(current_coordinate, E, "E", instructions, steps_queue)
-    if is_move_direction_permissible(current_coordinate[2], current_coordinate[3], "S"):
+    if is_move_direction_permissible(current_coordinate[2], current_coordinate[3], "S", p2):
         S = (current_coordinate[1][0] + 1, current_coordinate[1][1])
         try_adding_step_to_queue(current_coordinate, S, "S", instructions, steps_queue)
-    if is_move_direction_permissible(current_coordinate[2], current_coordinate[3], "W"):
+    if is_move_direction_permissible(current_coordinate[2], current_coordinate[3], "W", p2):
         W = (current_coordinate[1][0], current_coordinate[1][1] - 1)
         try_adding_step_to_queue(current_coordinate, W, "W", instructions, steps_queue)
 
-def is_move_direction_permissible(current_direction, current_direction_steps, proposed_direction):
-    if (current_direction == proposed_direction) and current_direction_steps == 3:
-        return False
+def is_move_direction_permissible(current_direction, current_direction_steps, proposed_direction, p2):
     if current_direction == "N" and proposed_direction == "S":
         return False
     if current_direction == "S" and proposed_direction == "N":
@@ -59,6 +58,17 @@ def is_move_direction_permissible(current_direction, current_direction_steps, pr
         return False
     if current_direction == "E" and proposed_direction == "W":
         return False
+    # Check part 1 conditions
+    if p2 == False:
+        if (current_direction == proposed_direction) and current_direction_steps == 3:
+            return False
+    if p2 == True:
+        if current_direction_steps < 4:
+            if current_direction != proposed_direction:
+                return False
+        if (current_direction == proposed_direction) and current_direction_steps == 10:
+            return False
+
     return True
 
 def try_adding_step_to_queue(current_coordinate, new_coordinate, new_direction, instructions, steps_queue):
@@ -78,7 +88,13 @@ def is_coordinate_valid(new_coordinate, instructions):
     return True
 
 def part2(instructions):
-    pass
+    visited_coordinates = set()
+    initial_coordinate = (0,0)
+    steps_queue = queue.PriorityQueue()
+    steps_queue.put((0, initial_coordinate, "S", 0))
+    steps_queue.put((0, initial_coordinate, "E", 0))
+    heat_loss = run_steps_calculation(instructions, visited_coordinates, steps_queue, True)
+    return heat_loss
 
 if __name__ == "__main__":
     instructions = get_input()
